3d13fc13e93f02b5fcff1dfd332bd845a2f6c32a
Vishnu KR <vishnukr1995@gmail.com>
2026-06-07 17:36:15 +0530

    Parameterize git branch and repository URL

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,6 +1,6 @@
 def call(String repoUrl, String branch) {
 
-    git branch: main,
-        url: https://github.com/vishnukrajan007/10-micro-service-helm.git
+    git branch: branch,
+        url: repoUrl
 
 }
